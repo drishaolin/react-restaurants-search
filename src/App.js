e369625e02e74793491b081e661830c6b12e8a10
Adriana Mucciolo <drishaolin@gmail.com>
2021-10-04 19:47:08 -0300

    create search input and styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import styled from 'styled-components';
 import { ThemeProvider } from 'styled-components';
 import { Reset } from 'styled-reset';
 
 import theme from './theme';
 import Home from './pages/Home';
 
-const Title = styled.h1`
-  font-size: 2.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
 function App() {
   return (
     <div >
-      <Title>React styled</Title>
       <ThemeProvider theme={theme}> 
       <Reset/>
       <Home/> 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,30 @@
-import { Container } from './styles';
+import { useState } from "react";
+import { Container, Logo, Search, Wrapper } from "./styles";
+import TextField, { Input } from "@material/react-text-field";
+import MaterialIcon from "@material/react-material-icon";
+
+import logo from "../../assets/logo.svg";
 
 export default function Home() {
-    return <Container> React Restaurant</Container>;
+    const [inputValue, setInputValue] = useState("");
+    return (
+        <Wrapper>
+            <Container>
+                <Search>
+                    <Logo src={logo} alt="logo restaurant find" />
+                    <TextField
+                        label="Pesquisar Restaurantes"
+                        outlined
+                        trailingIcon={<MaterialIcon role="button" icon="search" />}
+                    >
+                        <Input
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.currentTarget.value)}
+                        />
+                    </TextField>
+                </Search>
+            </Container>
+            
+        </Wrapper>
+    );
 }
