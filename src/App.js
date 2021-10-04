0677c65643e128f60c2762653503aab06313d955
Adriana Mucciolo <drishaolin@gmail.com>
2021-10-04 18:47:09 -0300

    add styled-reset, create home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import styled from 'styled-components';
+import { ThemeProvider } from 'styled-components';
+import { Reset } from 'styled-reset';
+
+import theme from './theme';
+import Home from './pages/Home';
 
 const Title = styled.h1`
   font-size: 2.5em;
@@ -10,6 +15,10 @@ function App() {
   return (
     <div >
       <Title>React styled</Title>
+      <ThemeProvider theme={theme}> 
+      <Reset/>
+      <Home/> 
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,5 @@
+import { Container } from './styles';
+
+export default function Home() {
+    return <Container> React Restaurant</Container>;
+}
